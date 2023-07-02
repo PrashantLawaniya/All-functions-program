be1dedf78503e2e21be8f84bc706df40aa39e786
PrashantLawaniya <138219435+PrashantLawaniya@users.noreply.github.com>
2023-07-02 22:48:48 +0530

    Create functionaverage.c

---
+++ functionaverage.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+float average(float a,float b);
+int main(){
+    printf(" the average is %f",average(2.1,8));
+    return 0;
+}
+float average(float a,float b)
+ {
+    float sum=a+b;
+    return (sum)/2;
+ }
+   
+
+   
+ 
