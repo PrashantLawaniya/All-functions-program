8003c0623e779491f35cc03d07c70c02c11bd71a
PrashantLawaniya <138219435+PrashantLawaniya@users.noreply.github.com>
2023-10-08 00:45:41 +0530

    Create Stringsprograms.c

---
+++ Stringsprograms.c
@@ -0,0 +1,9 @@
+int main()
+{
+    char str[] = "PrashantLawaniya";
+   printf("%s\n", str);
+  int length = 0;
+ length = strlen(str);
+ printf("Length of string str is %d", length);
+ return 0;
+}
